6cb322a073b1338f74441d8caa1540723136130f
unknown <zufar.ismanov@gmail.com>
2014-03-25 22:45:34 +0600

    Update Crime model

--- Model/Crime.php
+++ Model/Crime.php
@@ -2,8 +2,7 @@
 
 class Crime {
 
-	$con = mysqli_connect('localhost', 'ilya', 'database', 'test');
-	
+	private $con;
 	private $description;
     private $category;
     private $_date;
@@ -12,15 +11,22 @@ class Crime {
     private $latitude;
     private $longitude;
 	
-	function __construct($description, $category, $date, $time, $police_report, $latitude, $longitude) {
-        $this -> description = $description;
-        $this -> category = $category;
-        $this -> _date = $date;
-		$this -> _time = $time;
-        $this -> police_report = $police_report;
-        $this -> latitude = $latitude;
-        $this -> longitude = $longitude;
+	
+	function __construct() {
+        $this -> con = mysqli_connect('localhost', 'ilya', 'database', 'test');
     }
+	
+	function create($description, $category, $date, $time, $police_report, $latitude, $longitude) {	
+		$crime = new Crime();
+		$crime -> description = $description;
+        $crime -> category = $category;
+        $crime -> _date = $date;
+		$crime -> _time = $time;
+        $crime -> police_report = $police_report;
+        $crime -> latitude = $latitude;
+        $crime -> longitude = $longitude;
+		return $crime;
+    }	
 	
 	
 	public function insert() {
@@ -33,15 +39,15 @@ class Crime {
         $latitude = $this -> latitude;
         $longitude = $this -> longitude;
 		
-		$q = "INSERT INTO crimes (description, category, date, time, police_report, latitude, longitude) "
-		+ "VALUES ('$description', '$category', '$date', '$time', '$police_report', '$latitude', '$longitude')";
-		$cmd = mysqli_query($con, $q);
+		$q = "INSERT INTO crimes (description, category, date, time, police_report, latitude, longitude) VALUES ('$description', '$category', '$date', '$time', '$police_report', '$latitude', '$longitude')";
+		$cmd = mysqli_query($this -> con, $q);
 	
 	}
 	
-	public function fetch() {
-	
-		$cmd = mysqli_query($con,'SELECT * FROM crimes');
+	public function fetch() {		
+		
+		$con = mysqli_connect('localhost', 'ilya', 'database', 'test');
+		$cmd = mysqli_query($this -> con,'SELECT * FROM crimes');
 		$rows = array();
 		while($r = mysqli_fetch_assoc($cmd))
 		{
