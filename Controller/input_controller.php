44089912eab37abd52e847234b8a107746facc8a
unknown <zufar.ismanov@gmail.com>
2014-03-25 22:47:35 +0600

    Update controllers

--- Controller/input_controller.php
+++ Controller/input_controller.php
@@ -1,7 +1,6 @@
 <?php	
 
-	include("../Model/Model.php");
-	$db = new Database('localhost', 'ilya', 'database', 'test');
+	include("../Model/Crime.php");
 	if (isset($_POST['description']) && $_POST != "") {
 		$description = $_POST['description'];
 		$category = $_POST['category'];
@@ -10,6 +9,7 @@
 		$police_report = $_POST['police_report'];
 		$latitude = $_POST['latitude'];
 		$longitude = $_POST['longitude'];
-		$db->insert($description, $category, $date, $time, $police_report, $latitude, $longitude);
+		$crime = Crime::create($description, $category, $date, $time, $police_report, $latitude, $longitude);
+		$crime->insert();
 	}
 ?>
--- Controller/output_controller.php
+++ Controller/output_controller.php
@@ -1,6 +1,5 @@
 <?php		
-	include("../Model/Model.php");
-	$db = new Database('localhost', 'ilya', 'database', 'test');
-	$data = $db->select();
-	print json_encode($data);
+	include("../Model/Crime.php");
+	$crime = new Crime();
+	$crime -> fetch();
 ?>
