6684704098a2adc8558ecc202c733256e3678d9c
neeilya <neeilya.94@gmail.com>
2014-04-02 00:53:11 +0600

    Comment_controller 2nd try
    Comment_controller version 0.2 beta

--- Controller/Comment_controller.php
+++ Controller/Comment_controller.php
@@ -5,12 +5,15 @@
 		$comment = $_POST['comment'];
 		$commentor_name = $_POST['commentor_name'];
 		$crime_id= $_POST['crime_id'];
-		$rating= $_POST['rating'];
-		$comment = Comment::create($crime_id, $commentor_name, $comment, $rating);
+		$comment = Comment::create($crime_id, $commentor_name, $comment);
 		$comment->add();
 	}
 	else{
-		$comment = new Comment();
-		$comment -> fetch();
+		if (isset($_POST['crime_id']) && $_POST != "")) {
+			$comment = new Comment();
+			$crime_id = $_POST['crime_id'];
+			$comment -> getNewsComments($crime_id);
+		}
+		
 	}
 ?>
