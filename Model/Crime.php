0bf40a56c9392e635dba35d89ba8ab207a57f178
Zufar Ismanov <zufarismanov@MacBook-Pro-Zufar.local>
2014-04-01 23:50:56 +0600

    model update

--- Model/Crime.php
+++ Model/Crime.php
@@ -1,8 +1,9 @@
 <?php
 
-class Crime {
+include("Database.php");
 
-	private $con;
+class Crime {
+	private $db;
 	private $description;
     private $category;
     private $_date;
@@ -13,7 +14,7 @@ class Crime {
 	
 	
 	function __construct() {
-        $this -> con = mysqli_connect('localhost', 'ilya', 'database', 'test');
+		$this -> db = new Database();
     }
 	
 	function create($description, $category, $date, $time, $police_report, $latitude, $longitude) {	
@@ -37,25 +38,16 @@ class Crime {
 		$time = $this -> _time; 
         $police_report = $this -> police_report;
         $latitude = $this -> latitude;
-        $longitude = $this -> longitude;
-		
+        $longitude = $this -> longitude;		
 		$q = "INSERT INTO crimes (description, category, date, time, police_report, latitude, longitude) VALUES ('$description', '$category', '$date', '$time', '$police_report', '$latitude', '$longitude')";
-		$cmd = mysqli_query($this -> con, $q);
-	
+		$this -> db -> query($q);	
 	}
 	
-	public function fetch() {		
-		
-		$con = mysqli_connect('localhost', 'ilya', 'database', 'test');
-		$cmd = mysqli_query($this -> con,'SELECT * FROM crimes');
-		$rows = array();
-		while($r = mysqli_fetch_assoc($cmd))
-		{
-			$rows[] = $r;
-		}
-		print json_encode($rows);
-		
+	public function fetch() {
+		$q = "SELECT * FROM crimes";
+		$data = $this -> db ->fetch_all_array($q);
+		print json_encode($data);	
 	}
-}
 
+}
 ?>
