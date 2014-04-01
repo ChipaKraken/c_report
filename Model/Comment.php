a6a2ea627cf5542a33aa6aea00b8e0690f339220
Zufar Ismanov <zufarismanov@MacBook-Pro-Zufar.local>
2014-04-02 00:46:48 +0600

    updated comment model

--- Model/Comment.php
+++ Model/Comment.php
@@ -3,45 +3,57 @@
 include("Database.php");
 
 class Comment {
-	
-	private $db;
-	private $crime_id;
+    
+    private $db;
+    private $crime_id;
     private $commentor_name;
     private $comment;
     private $rating;
 
-	function __construct() {
+    function __construct() {
         $this -> db = new Database();
     }
-	
-	function create($crime_id, $commentor_name, $comment, $rating) {	
-		$comment = new Comment();
-		$comment -> crime_id = $crime_id;
+    
+    function create($crime_id, $commentor_name, $comment, $rating) {    
+        $comment = new Comment();
+        $comment -> crime_id = $crime_id;
         $comment -> commentor_name = $commentor_name;
         $comment -> comment = $comment;
-		$comment -> rating = $rating;
-		return $comment;
+        $comment -> rating = $rating;
+        return $comment;
     }
 
     function add() {
 
-    	$crime_id = $this -> crime_id;
-    	$commentor_name = $this -> commentor_name;
-    	$comment = $this -> comment;
-    	$rating = 0;
-    	$q = "INSERT INTO comments (crime_id, commentor_name, comment, rating) VALUES ('$crime_id', '$commentor_name', '$comment', '$rating')";
-    	$this -> db -> query($q);
+        $crime_id = $this -> crime_id;
+        $commentor_name = $this -> commentor_name;
+        $comment = $this -> comment;
+        $rating = 0;
+        $q = "INSERT INTO comments (crime_id, commentor_name, comment, rating) VALUES ('$crime_id', '$commentor_name', '$comment', '$rating')";
+        $this -> db -> query($q);
     }
 
     function getNewsComments($id) { 
 
-    	$q = "SELECT * FROM comments WHERE crime_id = $id";
-		$data = $this -> db ->fetch_all_array($q);
-		print json_encode($data);
+        $q = "SELECT * FROM comments WHERE crime_id = $id";
+        $data = $this -> db ->fetch_all_array($q);
+        print json_encode($data);
     }
 
     function changeRating($id, $value) {
 
-    	if($value == 0) {
-    		$q = "UPDATE comments SET rating = rating + 1 WHERE comment_id = %id";
-    		$this -> db -> que
+        if($value == 0) {
+            $q = "UPDATE comments SET rating = rating + 1 WHERE comment_id = %id";
+            $this -> db -> query($q);
+        }
+        else {
+            $q = "UPDATE comments SET rating = rating - 1 WHERE comment_id = %id";
+            $this -> db -> query($q);
+        }
+    }
+
+}
+
+?>
+
+
