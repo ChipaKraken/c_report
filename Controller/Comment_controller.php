470f909145c4c4a5594e453cd0f5720e174844f9
neeilya <neeilya.94@gmail.com>
2014-04-08 22:10:51 +0600

    Comment controller update
    Post request --> GET request

--- Controller/Comment_controller.php
+++ Controller/Comment_controller.php
@@ -1,25 +1,25 @@
 <?php	
 
 	include("../Model/Comment.php");
-	if (isset($_POST['comment']) && $_POST != "") {
-		$comment = $_POST['comment'];
-		$commentor_name = $_POST['commentor_name'];
-		$crime_id= $_POST['crime_id'];
+	if (isset($_GET['comment']) && $_POST != "") {
+		$comment = $_GET['comment'];
+		$commentor_name = $_GET['commentor_name'];
+		$crime_id= $_GET['crime_id'];
 		$comment = Comment::create($crime_id, $commentor_name, $comment);
 		$comment->add();
 	}
 	else{
-		if (isset($_POST['crime_id']) && $_POST != "")) {
+		if (isset($_GET['crime_id']) && $_POST != "") {
 			$comment = new Comment();
-			$crime_id = $_POST['crime_id'];
+			$crime_id = $_GET['crime_id'];
 			$comment -> getNewsComments($crime_id);
 		}
-		else if (isset($_POST['rating']) && $_POST != "") {
+		else if (isset($_GET['rating']) && $_POST != "") {
 			$comment = new Comment();
-			$comment_id = $_POST['comment_id'];
-			$value = $_POST['ratingValue'];
+			$comment_id = $_GET['comment_id'];
+			$value = $_GET['ratingValue'];
 		 	$comment -> changeRating($comment_id, $value);
 		}
-		
+
 	}
 ?>
