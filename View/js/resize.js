59168f6222efd96afc50169bdec9461b421a1c53
neeilya <neeilya.94@gmail.com>
2014-05-11 13:31:39 +0600

    resize script to fit makeup

---
+++ View/js/resize.js
@@ -0,0 +1,7 @@
+function resize() {
+	$("#map").height($(document).height() - 50);
+	$("#feed").height($(document).height() - 50);
+	$("#feedForm").height($(document).height() - 205);
+}
+
+//window.onresize = resize();
