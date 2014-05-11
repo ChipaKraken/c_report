5bee33f26f345f8f7edcc2a4746577f5a333d7ac
neeilya <neeilya.94@gmail.com>
2014-05-11 13:33:45 +0600

    correct date view when changing categories

--- View/js/category.js
+++ View/js/category.js
@@ -44,14 +44,21 @@ function func() {
             i = crime[_i];
             temp = i['time'].split(':');
             i['time'] = temp[0] + ':' + temp[1];
-            i['date'] = moment(i['date'], "YYYY-MM-DD").fromNow();
+
+            cdate = i['date'] + '-' + temp[0] + '-' + temp[1];
+            i['fromNow'] = moment(cdate, "YYYY-MM-DD-hh-mm").fromNow();
+            i['date'] = checkDate(i['date']);
+
             var temp = i['description'];
+            i['description'] = checkLength(i['description']);
             temp = temp.split(' ');
             i['category'] = convert_category(i['category']);
             if (temp.length >= 3) {
-                var popup_message = '<b>' + i['category'] + '</b><br />' + temp[0] + ' ' + temp[1] + ' ' + temp[2] + '<br />' + '<a onclick=comma("' + i['crime_id'] + '")>\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
-            } else {
-                var popup_message = '<b>' + i['category'] + '</b><br />' + temp[0] + '<br />' + '<a onclick=comma("' + i['crime_id'] + '")>\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
+                var popup_message = '<b>' + i['category'] + '</b><br />' + temp[0] + ' ' + temp[1] + ' ' + temp[2]  + '</div>' + '<br />' + '<a onclick=comma("' + i['crime_id'] + '") style="cursor: pointer;">\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
+            } else if(temp.length == 2) {
+				var popup_message = '<b>' + i['category'] + '</b><br />' + temp[0] + ' ' + temp[1] + '<br />' + '<a onclick=comma("' + i['crime_id'] + '") style="cursor: pointer;">\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
+			} else {
+                var popup_message = '<b>' + i['category'] + '</b><br />' + temp[0] + '<br />' + '<a onclick=comma("' + i['crime_id'] + '") style="cursor: pointer;">\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
             }
             marker = L.marker([
                 i['latitude'],
@@ -60,20 +67,24 @@ function func() {
             myMarkers.push(marker)
             $('#feedForm').append(news(i));
         }
-        //mouse over function (getting another marker above)
-        $('.newsItem')
-        .mouseover(function(a) {
+        $('.newsItem').click(function(a) {
             var crime_url = "Controller/Crime_controller?crime_id=" + a.toElement.id;
             var crime = JSON.parse(httpGet(crime_url));
-            var redIcon = new LeafIcon({iconUrl: 'js/images/marker-red.png'});
-            tmpMarker = L.marker([crime[0].latitude, crime[0].longitude], {icon: redIcon}).addTo(map).bindPopup("I am a red leaf.");
+            var icon_blue = new LeafIcon({iconUrl: 'js/images/marker-icon.png'});
+			var cat_pop = convert_category(crime[0].category);
+			var mess_pop = crime[0].description;
+			mess_pop = mess_pop.split(' ');
+            if (mess_pop.length >= 3) {
+                var popup_message = '<b>' + cat_pop + '</b><br />' + mess_pop[0] + ' ' + mess_pop[1] + ' ' + mess_pop[2]  + '</div>' + '<br />' + '<a onclick=comma("' + crime[0].crime_id + '") style="cursor: pointer;">\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
+            } else if(mess_pop.length == 2){
+                var popup_message = '<b>' + cat_pop + '</b><br />' + mess_pop[0] + ' ' +  mess_pop[1] + '<br />' + '<a onclick=comma("' + crime[0].crime_id + '") style="cursor: pointer;">\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
+            } else {
+				var popup_message = '<b>' + cat_pop + '</b><br />' + mess_pop[0] + '<br />' + '<a onclick=comma("' + crime[0].crime_id + '") style="cursor: pointer;">\u0427\u0438\u0442\u0430\u0442\u044c \u0434\u0430\u043b\u044c\u0448\u0435 \u2192</a>'
+			}
+            tmpMarker = L.marker([crime[0].latitude, crime[0].longitude], {icon: icon_blue}).addTo(map).bindPopup(popup_message);
+			tmpMarker.openPopup();
+			myMarkers.push(tmpMarker);
             map.setView(new L.LatLng(crime[0].latitude, crime[0].longitude), 13);
-        })
-        .mouseout(function() {
-            map.removeLayer(tmpMarker);
-        });        
-        $('.newsItem').click(function(a) {
-            comma(a.toElement.id);
         });
   }
 };
